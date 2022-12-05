07ed903c2f7f567bc1d53098cdd300e196d93607
starydv7 <pawanstar72926@gmail.com>
2022-12-05 22:57:33 +0530

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import styles from "./signup.module.css";
 const Login = () => {
+      const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const handleLogin = async() => {
+      let result = await fetch("http://localhost:5000/login", {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: {
+          "Content-Type": 'application/json'
+        }
+      });
+      result = await result.json();
+      console.log(result);
+       
+    }
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="Enter Email..."
+        className={styles.inputbox}
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <input
+        type="text"
+        placeholder="Enter Password..."
+        className={styles.inputbox}
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+          <button className={styles.btntag} onClick={ handleLogin}>
+        Login
+      </button>
+    </div>
+  );
 }
 
 export default Login;
