dc6c672c6fda1c52b115f745285bafba9287d04a
starydv7 <pawanstar72926@gmail.com>
2022-12-08 14:41:14 +0530

    login

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -2,22 +2,24 @@ import React, { useState } from 'react'
 import axios from "axios";
 import { useEffect } from 'react';
 const FetchData = () => {
-    const [data, setData] = useState("");
+    const [data, setData] = useState([]);
     useEffect(() => {
-      const fetchData = () => {
-          axios.get("http://localhost:4000/register")
-              .then((res) => {
-                  setData(res.data);
-          })
-      };
-    },[]);
+       function getData() {
+         fetch("http://localhost:4000/register").then((result) => {
+           result.json().then((resp) => {
+             console.log("result", resp);
+             setData(resp);
+           });
+         });
+       }
+    })
   return (
       <div>
           {data.map((item) => {
-              <div>
-                  {item.email}
-              </div>
-          })}
+              return (
+                  <div>{item.email}</div>
+             )
+         })}
           
     </div>
   )
